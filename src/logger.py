1e5262534cd9713dc8e8c545f666f9171329fca9
Ramki Sridhar <ramkisridhar2001@gmail.com>
2026-02-06 08:31:30 +0530

    Logger and exception fixes

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime 
 
 #Log file naming:
-LOG_FILE = f"{datetime.now().strftime('%m_%d_Y_%H_%M_%S')}.log"
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 # Create logs directory (ONLY directory here)
 logs_path = os.path.join(os.getcwd(), 'logs')
