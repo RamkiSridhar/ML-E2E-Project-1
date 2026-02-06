b3fa7a528a24ad1632cb9d3fabf7921a5ca249a5
Ramki Sridhar <ramkisridhar2001@gmail.com>
2026-02-06 08:25:10 +0530

    Logger and exception files added

---
+++ src/logger.py
@@ -0,0 +1,25 @@
+import logging 
+import os 
+from datetime import datetime 
+
+#Log file naming:
+LOG_FILE = f"{datetime.now().strftime('%m_%d_Y_%H_%M_%S')}.log"
+
+# Create logs directory (ONLY directory here)
+logs_path = os.path.join(os.getcwd(), 'logs')
+os.makedirs(logs_path, exist_ok= True)
+
+# Full log file path
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename= LOG_FILE_PATH, 
+    format= "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s", 
+    level= logging.INFO, 
+    force= True
+)
+
+#TESTING:
+
+# if __name__ == '__main__':
+#     logging.info("Logging has started.")
